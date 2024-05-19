3491831fb79f337a209b50e242800682cfc81c29
SeungA Lee <dltmddk981119@gmail.com>
2024-05-19 23:42:34 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ JavaScript/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,8 +1,7 @@
 function solution(numbers) {
-    
-  let doubleEl = [];
-    for(let i = 0; i < numbers.length; i++) {
-        doubleEl.push( numbers[i] * 2);
+    const answer = [];
+    for (let i = 0; i < numbers.length; i++) {
+        answer.push(numbers[i] * 2);
     }
-    return doubleEl;
+    return answer;
 }

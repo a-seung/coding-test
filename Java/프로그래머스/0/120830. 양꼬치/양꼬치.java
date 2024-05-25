4ae0aa658681d7da2ad63737b5042c66cb456523
SeungA Lee <dltmddk981119@gmail.com>
2024-05-25 17:38:41 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- Java/프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ Java/프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int n, int k) {
-        int price = n * 12000 + k * 2000;
-        int cola = (int) (n / 10);
-        int answer = price - (cola * 2000);
-        return answer;
+       
+        int s = n / 10;
+        int price = n * 12000 + k * 2000 - s * 2000;
+        return price;
     }
 }

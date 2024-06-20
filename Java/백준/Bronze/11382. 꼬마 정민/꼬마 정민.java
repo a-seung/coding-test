b942695aad6188676f7d662063c48dfcf802a259
SeungA Lee <dltmddk981119@gmail.com>
2024-06-20 22:56:23 +0900

    [Bronze V] Title: 꼬마 정민, Time: 96 ms, Memory: 14112 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // A,B,C는  1이상 10^12 이하 정수이므로 long타입 써줘야함
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
+        long sum = A + B + C;
+
+        System.out.println(sum);
+    }
+}

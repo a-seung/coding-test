9239766d7f7e782d807c51cb7935b0393889b2c5
SeungA Lee <dltmddk981119@gmail.com>
2024-06-20 23:16:16 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 14032 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10172. 개/개.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|"); // 백슬래시 쓰려면 하나 더 써줘야함
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\"); //큰따옴표 쓰려면 앞에 백슬래시 넣어줘야함
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}

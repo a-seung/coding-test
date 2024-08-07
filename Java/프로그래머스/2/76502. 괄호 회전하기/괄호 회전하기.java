0410647542a8276e764c596e695e433632fd4829
SeungA Lee <dltmddk981119@gmail.com>
2024-08-07 18:28:56 +0900

    [level 2] Title: 괄호 회전하기, Time: 23.45 ms, Memory: 84 MB -BaekjoonHub

--- Java/프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ Java/프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -5,8 +5,8 @@ class Solution {
         String doubleS = s + s;
         int answer = 0;
         
-        for(int i = 0; i < s.length(); i++) {
-            if(isValid(doubleS.substring(i, s.length() + i))) {
+        for (int i = 0; i < s.length(); i++) {
+            if (isValid(doubleS.substring(i, s.length() + i))) {
                 answer++;
             }
         }
@@ -17,7 +17,7 @@ class Solution {
         Deque<Character> stack = new ArrayDeque<>();
         
         for (char current : s.toCharArray()) {
-            if ( current == '(' || current == '{' || current == '[' ) {
+            if (current == '(' || current == '{' || current == '[') {
                 stack.push(current);
             } else {
                 if(stack.isEmpty()) {
@@ -25,13 +25,13 @@ class Solution {
                 }
                 
                 char target = stack.pop();
-                if ((target == '(' && current != ')') ||
-                    (target == '{' && current != '}') ||
-                    (target == '[' && current != ']')) {
-                    return false;              
-                }            
+                if((target == '(' && current != ')') ||
+                   (target == '{' && current != '}') ||
+                   (target == '[' && current != ']')) {
+                    return false;
+                }
             }
         }
-       return stack.isEmpty();
+        return stack.isEmpty();
     }
 }

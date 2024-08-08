e63b7999120f16fb0fc73e75df2b49c4c8b1ba94
SeungA Lee <dltmddk981119@gmail.com>
2024-08-08 18:18:13 +0900

    [level 2] Title: 주식가격, Time: 29.74 ms, Memory: 75.7 MB -BaekjoonHub

--- Java/프로그래머스/2/42584. 주식가격/주식가격.java
+++ Java/프로그래머스/2/42584. 주식가격/주식가격.java
@@ -7,16 +7,17 @@ class Solution {
         
         for (int i = 0; i < prices.length; i++) {
             while(!stack.isEmpty()) {
-                 int j = stack.peek();
-                  if (prices[j] > prices[i]) {
-                      answer[j] = i - j;
-                      stack.pop();
-                } 
+                int j = stack.peek();
+                if(prices[j] > prices[i]) {
+                    answer[j] = i - j;
+                    stack.pop();
+                }
                 else break;
             }
             stack.push(i);
         }
-        while (!stack.isEmpty()) {
+        
+        while(!stack.isEmpty()){
             int i = stack.pop();
             answer[i] = prices.length - i - 1;
         }

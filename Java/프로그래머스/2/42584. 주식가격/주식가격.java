a7fe046bf3e0d839e8b933ebb48b1ee3c4594549
SeungA Lee <dltmddk981119@gmail.com>
2024-08-08 17:50:14 +0900

    [level 2] Title: 주식가격, Time: 27.89 ms, Memory: 73 MB -BaekjoonHub

--- Java/프로그래머스/2/42584. 주식가격/주식가격.java
+++ Java/프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,22 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
         Deque<Integer> stack = new ArrayDeque<>();
+        int[] answer = new int[prices.length];
         
         for (int i = 0; i < prices.length; i++) {
             while(!stack.isEmpty()) {
-                int j = stack.peek();
-                if(prices[j] > prices[i]) {
-                    answer[j] = i - j;
-                    stack.pop();
-                }
+                 int j = stack.peek();
+                  if (prices[j] > prices[i]) {
+                      answer[j] = i - j;
+                      stack.pop();
+                } 
                 else break;
             }
             stack.push(i);
         }
-        
-        while(!stack.isEmpty()) {
+        while (!stack.isEmpty()) {
             int i = stack.pop();
             answer[i] = prices.length - i - 1;
         }

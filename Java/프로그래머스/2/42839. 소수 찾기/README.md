e19ef4b8439831d1d6437013da5c2f6f0fab8120
SeungA Lee <dltmddk981119@gmail.com>
2024-08-27 17:03:26 +0900

    [level 2] Title: 소수 찾기, Time: 13.70 ms, Memory: 80.2 MB -BaekjoonHub

--- Java/프로그래머스/2/42839. 소수 찾기/README.md
+++ Java/프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 11.04 ms
+메모리: 80.2 MB, 시간: 13.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 27일 12:38:55
+2024년 08월 27일 17:03:24
 
 ### 문제 설명
 
--- Java/프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ Java/프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,23 +2,22 @@ import java.util.*;
 
 class Solution {
     Set<Integer> candidates = new HashSet<>();
-        public int solution(String numbers) {
-            //1. 재귀로 순열조합 생성
-            permutation(numbers, 0, new boolean[numbers.length()], 0);
-            
-            //2. 소수인지 확인
-            int answer = 0;
-            for(int num : candidates) {
-                if (isPrime(num)) answer++;
-            }
-            return answer;
+    public int solution(String numbers) {
+        permutation(numbers, 0, new boolean[numbers.length()], 0);
+        
+        // 소수 판별
+        int answer = 0;
+        for (int num : candidates) {
+            if(isPrime(num)) answer++;
+        }
+        return answer;
     }
     
-    void permutation(String numbers,int current, boolean[] visited, int digit) {
-        if (digit == numbers.length()) return;
+    void permutation(String numbers, int current, boolean[] visited, int digit) {
+        if(digit == numbers.length()) return;
         
-        for (int i = 0; i < numbers.length(); i++) {
-            if (visited[i]) continue;
+        for(int i = 0; i < numbers.length(); i++) {
+            if(visited[i]) continue;
             
             int newValue = current + (int)((numbers.charAt(i) - '0') * Math.pow(10, digit));
             candidates.add(newValue);
@@ -29,12 +28,13 @@ class Solution {
         }
     }
     
-    boolean isPrime (int n) {
+    boolean isPrime(int n) {
         if (n < 2) return false;
         
         for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
+            if(n % i == 0) return false;
         }
-        return true;
+         return true;
     }
+   
 }
